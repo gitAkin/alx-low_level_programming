5f169a5065cfc3d1025000c37a50fe936a408ad8
gitAkin <akinwumialalade@yahoo.com>
2023-09-07 14:32:57 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main- this is a c programe that prints exactly the required output
+ *
+ *Return: By default returns 0 if no errors otherwise returns non 0 value.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
