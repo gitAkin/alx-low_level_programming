0ab2bf25e18a40997e1b8cbdc43bc2b23a3283b1
gitAkin <akinwumialalade@yahoo.com>
2023-09-07 15:19:46 -0700

    100-intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+/**
+  *main-this is the program that prfints that piece of art and more.
+  *
+  *Return:(0) if errors, and non(0) if no errors
+  */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
