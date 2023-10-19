ffec56e1aa10204a1934273b44f31fbebafdc439
gitAkin <akinwumialalade@yahoo.com>
2023-10-19 13:34:12 -0700

    function that adds a new node at the beginning of a list_t list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+
+/**
+ * add_node - This function adds a new node at the beginning
+ * of a list_t list.
+ * @head: This the the head of the linked list.
+ * @str: string to store in the list.
+
+ * Return: address of the head.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *akin;
+	size_t akins;
+
+	akin = malloc(sizeof(list_t));
+	if (akin == NULL)
+		return (NULL);
+
+	akin->str = strdup(str);
+
+	for (akins = 0; str[akins]; akins++)
+		;
+
+	akin->len = akins;
+	akin->next = *head;
+	*head = akin;
+
+	return (*head);
+}
